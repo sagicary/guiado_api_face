4e43879dcfd8d2a3c652bf8bea56de73fc6fe9e9
sagicary <sagicary@gmail.com>
2017-07-05 13:00:47 -0400

    Pruebas

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+
+
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+
+
+
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
